04171786cb136348fc38d42f653cdfc993393911
MJanuszek <monika.januszek.mail@gmail.com>
2023-03-31 11:11:16 +0200

    Working on place bet function

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const squares = document.querySelectorAll("div.square");
-const cash = document.querySelector(".cash");
 const play = document.querySelector(".play");
+const betResult = document.querySelector(".bet-result");
+const money = document.querySelector(".money");
+let amount = document.getElementById("amount");
 
+let bet = 0;
 
+amount.addEventListener("change", () => {
+    bet = amount.value;
+});
 let number = 200;
 let won = 0;
 let colorsDrawn = [];
+money.textContent = `Twoje środki: ${number}`
 
 const changeColors = () => {
     const colors = [{name: "red", id:1},{name: "green", id:2}, {name: "blue", id:3}];
@@ -23,19 +30,36 @@ const compareColorResult = () => {
     let result = [...new Set(colorsDrawn)]
     console.log(result);
     if(result.length === 3){
-        console.log("Przegrałeś 10$");
-    } else if(result.length === 2) {
-        console.log("Wygrałeś 1$");
+        number -= bet;
+        betResult.textContent = `Przegrałeś ${bet} `;
+        money.textContent = `Twoje środki: ${number}`
+    } else if(result.length === 2){
+        betResult.textContent = `Ani straty ani zysku `;
     } else if(result.length === 1){
-        console.log("Wygrałeś 100$");
+        number += bet;
+        betResult.textContent = `Wygrałeś ${bet} `;
+        money.textContent = `Twoje środki: ${number}`
     }
     colorsDrawn = [];
+    
 }
 
+ const checkAllNumbers = () => {
+    if(amount.value === "") {
+        betResult.textContent = "Postaw zakład"
+        return
+    }
+    if(number <=0){
+        console.log("Masz za mało środków")
+    }
+ }
+
 const startGame = () => {
+    checkAllNumbers()
+    betResult.textContent = `Wynik: `
+    amount.value = "";
     changeColors();
     compareColorResult();
 }
 
-cash.textContent = `${number} $`;
 play.addEventListener("click", startGame);
